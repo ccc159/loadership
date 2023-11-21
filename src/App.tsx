5f54a7a7020af6f411bd3ec463f213447173896e
Jingcheng Chen <mail@jchen.ch>
2023-11-21 20:44:53 +0100

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <p className='text-3xl font-bold text-yellow-300'>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
