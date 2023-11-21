84576be774c99ea84b35fb4bdacfea48d1c6828e
Jingcheng Chen <mail@jchen.ch>
2023-11-21 20:30:58 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
