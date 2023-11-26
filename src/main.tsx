3e4d2cdba6c3a856f301255558e94c135a991223
Jingcheng Chen <mail@jchen.ch>
2023-11-26 11:19:09 +0100

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { LicensePage } from './LicensePage.tsx';
 import { LoaderDotLinearComponent } from './loaders/LoaderDotLinear.tsx';
 import { Header } from './UI/Header.tsx';
 import { NotificationComponent } from './components/Notification.tsx';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='*' element={<ErrorPage />} />
       </Routes>
       <NotificationComponent />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
