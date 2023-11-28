14a54281f468026ef2a824432524957523dc908b
Jingcheng Chen <mail@jchen.ch>
2023-11-28 16:52:24 +0100

    added dot bounce loader

---
+++ src/loaders/LoaderDotLinearBounce.tsx
@@ -0,0 +1,172 @@
+import { Configurator } from '../UI/Configurator';
+import { generateShortID } from '../utils';
+import { LoaderClass } from './Loader';
+
+export class LoaderDotLinearBounceClass extends LoaderClass {
+  public params: {
+    autoLoaderSize: boolean;
+    loaderWidth: number;
+    loaderHeight: number;
+    loaderVersion: string;
+    dotNum: number;
+    dotSize: number;
+    dotDistance: number;
+    dotColor: string;
+    bounceHeight: number;
+    speed: number;
+    pause: number;
+  };
+
+  constructor() {
+    super();
+    this.params = {
+      autoLoaderSize: true,
+      loaderWidth: 0,
+      loaderHeight: 0,
+      loaderVersion: generateShortID(),
+      dotNum: 4,
+      dotSize: 15,
+      dotDistance: 24,
+      dotColor: '#ffffff',
+      bounceHeight: 40,
+      speed: 1.6,
+      pause: 0.8,
+    };
+    this.controls = {
+      ...this.controls,
+      dotNum: {
+        name: 'Number of dots',
+        type: 'number',
+        group: 'Dot',
+        forceUpdate: true,
+        affectLoaderSize: true,
+      },
+      dotSize: {
+        name: 'Dot size',
+        type: 'number',
+        group: 'Dot',
+        unit: 'px',
+        affectLoaderSize: true,
+      },
+      dotDistance: {
+        name: 'Dot distance',
+        type: 'number',
+        group: 'Dot',
+        unit: 'px',
+        affectLoaderSize: true,
+      },
+      dotColor: {
+        name: 'Dot color',
+        type: 'color',
+        group: 'Dot',
+      },
+      bounceHeight: {
+        name: 'Bounce height',
+        type: 'number',
+        group: 'Dot',
+        unit: 'px',
+      },
+      speed: {
+        name: 'Speed',
+        type: 'number',
+        group: 'Speed',
+        min: 0,
+        max: 10,
+        step: 0.01,
+        unit: 's',
+      },
+      pause: {
+        name: 'Pause time',
+        type: 'number',
+        group: 'Speed',
+        min: 0,
+        max: 5,
+        step: 0.01,
+      },
+    };
+  }
+
+  public override get PerfectWidth(): number {
+    return this.params.dotDistance * (this.params.dotNum - 1) + this.params.dotSize;
+  }
+
+  public override get PerfectHeight(): number {
+    return this.params.dotSize + this.params.bounceHeight;
+  }
+
+  public override get HTML(): JSX.Element {
+    return (
+      <div className={`loadership_${this.params.loaderVersion}`}>
+        {Array(this.params.dotNum)
+          .fill(0)
+          .map((_, i) => (
+            <div key={i}></div>
+          ))}
+      </div>
+    );
+  }
+
+  public override get CSS(): string {
+    const tempStyles = Array(this.params.dotNum)
+      .fill(0)
+      .map(
+        (_, i) =>
+          `.loadership_${this.params.loaderVersion} div:nth-child(${i + 1}) {
+              animation-delay: ${((this.params.speed / this.params.dotNum) * i).toFixed(2)}s;
+              left: ${(this.params.loaderWidth - this.PerfectWidth) / 2 + i * this.params.dotDistance}px;
+          }`
+      )
+      .join('\n');
+
+    const actualAnimationPercent = this.params.speed / (this.params.speed + this.params.pause);
+
+    const styles = `
+        .loadership_${this.params.loaderVersion} {
+          display: flex;
+          position: relative;
+          width: ${this.params.loaderWidth}px;
+          height: ${this.params.loaderHeight}px;
+        }
+
+        .loadership_${this.params.loaderVersion} div {
+          position: absolute;
+          top: ${(this.params.loaderHeight - this.PerfectHeight) / 2 + this.params.bounceHeight}px;
+          width: ${this.params.dotSize}px;
+          height: ${this.params.dotSize}px;
+          border-radius: 50%;
+          background: ${this.params.dotColor};
+          animation: loadership_${this.params.loaderVersion}_bounce ${this.params.speed + this.params.pause}s infinite;
+          animation-timing-function: cubic-bezier(0.22, 0.61, 0.36, 1);
+        }
+
+        ${tempStyles}
+
+
+        @keyframes loadership_${this.params.loaderVersion}_bounce {
+          0%, ${20 * actualAnimationPercent}%, ${50 * actualAnimationPercent}%, ${80 * actualAnimationPercent}%, ${100 * actualAnimationPercent}%, 100% {
+            transform: translateY(0);
+          }
+        
+          ${40 * actualAnimationPercent}% {
+            transform: translateY(-${this.params.bounceHeight}px);
+          }
+        
+          ${60 * actualAnimationPercent}% {
+            transform: translateY(-${0.4 * this.params.bounceHeight}px);
+          }
+        }
+    `;
+    return styles;
+  }
+}
+
+const loader = new LoaderDotLinearBounceClass();
+const name = 'Loader Dot Linear Bounce';
+
+export const LoaderDotLinearBounce: ILoader = {
+  name,
+  slug: 'loader_dot_linear_bounce',
+  date: new Date('2023/11/22'),
+  component: <Configurator loader={loader} name={name} />,
+  preview: <Configurator loader={loader} preview />,
+};
--- src/loaders/Loaders.tsx
+++ src/loaders/Loaders.tsx
@@ -2,5 +2,6 @@ import { LoaderDotLinear } from './LoaderDotLinear';
 import { LoaderDotCircularScale } from './LoaderDotCircularScale';
 import { LoaderDotCircularPulse } from './LoaderDotCircularPulse';
 import { LoaderDotSquareGrid } from './LoaderDotSquareGrid';
+import { LoaderDotLinearBounce } from './LoaderDotLinearBounce';
 
-export const Loaders = [LoaderDotLinear, LoaderDotCircularScale, LoaderDotCircularPulse, LoaderDotSquareGrid];
+export const Loaders = [LoaderDotLinear, LoaderDotCircularScale, LoaderDotCircularPulse, LoaderDotSquareGrid, LoaderDotLinearBounce];
